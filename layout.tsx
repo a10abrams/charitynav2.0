a7710102a4f9bcaa28bb8c543498fed7b6d26e44
a10abrams <108836278+a10abrams@users.noreply.github.com>
2023-11-22 14:29:15 -0500

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,3 +1,5 @@
+import './/styles.css'
+
 export const metadata = {
   title: 'The News Trend-Based Nonprofit Index',
   description: 'Your trusted source for comprehensive insights into charitable organizations focusing on humanist topics in the news. At our heart, we are a beacon of hope and compassion, curating information on nonprofits responding to the pressing challenges of our time.',

ec077faedde4e7d661da61debb64cca3cf3e3277
a10abrams <108836278+a10abrams@users.noreply.github.com>
2023-11-28 15:40:49 -0500

    Update page.tsx
    Added header component
    
    ////side note, still getting 404 error when trying to navigate? legacyBehavior, <a> vs <Link> doesn't make a difference, changing the entire structure of the project....wondering if there is some sort of bug in nextjs?

--- page.tsx
+++ page.tsx
@@ -1,41 +1,7 @@
 export default function Home() {
     return (
     <>
-<div id="homepg_top_banner">
-    <p id="banner_msg">
-      Support{" "}
-      <a href="#" id="banner_link">
-        [insert cause here].
-      </a>
-    </p>
-  </div>
-  <nav id="nav_bar">
-    <div className="nav_link" id="index_logo">
-      <a href="ggxcn_home.html">
-        <img src="the_index_logo.jpg" alt="Nonprofit Index home" id="logo" />
-      </a>
-    </div>
-    <ul id="nav_list_container">
-      <li className="nav_link">
-        <a href="about.html">About</a>
-      </li>
-      <li className="nav_link">
-        <a href="nonprofits.html">Nonprofits</a>
-      </li>
-      <li className="nav_link" id="search">
-        <input type="search" id="search_bar" />
-        <button type="submit" id="search_button">
-          <span className="material-symbols-outlined">search</span>
-        </button>
-      </li>
-      <li className="nav_link">
-        <a href="signup.html">Sign up</a>
-      </li>
-      <li className="nav_link">
-        <a href="login.html">Log in</a>
-      </li>
-    </ul>
-  </nav>
+    <Header/>
   <div className="container">
     <img src="clay-banks-Pcg6X4QG63M-unsplash.jpg" id="site_img" />
     {/*onhover make attribute + link to cause in pic*/}

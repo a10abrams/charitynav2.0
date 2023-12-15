5381024447839ffbb420b5259d49707145d62f64
a10abrams <108836278+a10abrams@users.noreply.github.com>
2023-12-15 15:28:27 -0500

    Create search_page.tsx

---
+++ src/pages/search/search_page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import NavBar from '../navigation'
+
+export default function Search() {
+    return (
+        <>
+        <div id = "container">
+            <NavBar/>
+            <div id = "site_content">
+                <h1>Welcome to the search page.</h1>
+                <p>lorem lorem</p>
+            </div>
+        </div>
+       </>
+    );
+}

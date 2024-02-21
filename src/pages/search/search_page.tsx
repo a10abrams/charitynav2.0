dc7d460a69d2601f2a68877026911a157049b8e3
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-21 11:54:15 -0500

    Update search_page.tsx
    - Update all of search_page

--- src/pages/search/search_page.tsx
+++ src/pages/search/search_page.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
-import NavBar from '../components/navigation'
+import NavBar from '../../components/navigation'
+import SupportBanner from '@/components/support_banner';
+import Footer from '../components/footer'
 
 export default function Search() {
     return (
         <>
         <div id = "container">
             <NavBar/>
+            <SupportBanner/>
             <div id = "site_content">
                 <h1>Welcome to the search page.</h1>
                 <p>lorem lorem</p>

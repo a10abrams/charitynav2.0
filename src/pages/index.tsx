09b3d83c211515da1e9247cd83fc165d4d86be8d
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-21 11:15:14 -0500

    Update index.tsx
    - Remove wordiness
    - Organize code
    - Check for API here, too

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,18 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import Navbar from './navigation'
-import { getGalleryPhotos } from '../lib/api'
-import React, {useState} from 'react'
+import { useGlobalGivingData } from './api/api'
+import { useGalleryPhotos } from '../lib/hooks/useGalleryPhotos'
+import React, { useEffect, useState} from 'react'
+import Navbar from '../components/navigation'
+import SupportBanner from '../components/support_banner'
+import Footer from '../components/footer'
+import CarouselComponent from '@/components/carousel_component'
 
-export default function Home() {
+const Home = () => {
+  const { galleryPhotos } = useGalleryPhotos(5);
+
+  // JSX structure for Home page
   return (
     <>
         <Head>
@@ -18,33 +25,40 @@ export default function Home() {
         </Head>
         <div id = "container">
           <Navbar/>
+          <SupportBanner/>
           <div id = "site_content">
             <div id = "home_gallery">
               
             </div>
             <div className = "container_pg_layout_01"> 
               <div className = "left_gallery_layout_01">
-              {/* container for api info  */}
+                <CarouselComponent photos={ galleryPhotos }/>
               </div>
               <div className = "article_layout_01">
                 <h1 className = "article_title_layout_01">
                   Welcome to The News Trend-Based Nonprofit Index.
                 </h1>
                 <p className = "article_content_layout_01" id = "home_blurb">
-                  Empowering causes isn&apos;t just a wish; it&apos;s a choice we make. If you&apos;re uncertain about where to begin, start here! Dive into the heart of current
-                  events and discover the humans making a difference. Your click can be the
+                  Empowering causes isn&apos;t just a wish; it&apos;s a choice we make. Dive into the heart of current
+                  events and discover the humans making a difference.
+                  <span id = "opening_cta">Your click can be the
                   catalyst for change -&#160;
-                  <Link href="#" id="search_inline">
+                  <Link href = "/search/search_page" id = "search_inline">
                     how will you respond?
                   </Link>
+                  </span>
                 </p>
               </div>
               <div className = "right_gallery_layout_01">
-                {/* container for api info */}   
+                {/* container for api info */}
+                
               </div>
             </div>
           </div>
+          <Footer/>
         </div>
     </>
   )
-}
+};
+
+export default Home;

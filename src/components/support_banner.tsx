1ee540c5430e9cdc16a9471b8e18471405ac4d61
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-21 11:19:13 -0500

    Create support_banner.tsx

---
+++ src/components/support_banner.tsx
@@ -0,0 +1,14 @@
+export default function SupportBanner() {
+    return (
+        <>
+            <div id="homepg_top_banner">
+                <p id="banner_msg">
+                    Support{" "}
+                <a href="#" id="banner_link">
+                    [insert cause here].
+                </a>
+                </p>
+            </div>
+        </>
+    )
+}

61c33079da564efdf24c3dd6b486e20943817f44
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-21 11:16:29 -0500

    Update nonprofits_page.js
    - Update format and content

--- src/pages/nonprofits/nonprofits_page.js
+++ src/pages/nonprofits/nonprofits_page.js
@@ -1,16 +1,37 @@
 import Link from 'next/link'
-import NavBar from '../navigation'
+import NavBar from '../../components/navigation'
+import SupportBanner from '../../components/support_banner'
+import Footer from '../../components/footer'
 
 export default function Nonprofits() {
     return (
         <>
-            <div id = "container">
-                    <NavBar/>
-                    <div id = "site_content">
-                        <h1>Welcome to the nonprofits page.</h1>
-                        <p>lorem lorem</p>
-                    </div>
+        <div id = "container">
+            <NavBar/>
+            <SupportBanner/>
+            <div id = "site_content">
+            <div id = "home_gallery">
+              
             </div>
+            <div className = "container_pg_layout_01"> 
+              <div className = "left_gallery_layout_01">
+              {/* container for api info  */}
+              
+              </div>
+              <div className = "article_layout_01">
+                <h1 className = "article_title_layout_01">
+                  Nonprofits
+                </h1>
+                <p className = "article_content_layout_01" id = "np_blurb">
+                    lorem lorem
+                </p>
+              </div>
+              <div className = "right_gallery_layout_01">
+              </div>
+            </div>
+          </div>
+          <Footer/>
+        </div>
         </>
     );
 }

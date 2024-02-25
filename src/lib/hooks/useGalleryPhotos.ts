52f6e0f48278eb4d2635731587e3cc1ff83f45fb
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-25 14:58:57 -0500

    Update useGalleryPhotos.ts
    - removed unnecessary import, added necessary ones
    - changed the usage of getRandomGalleryPhotos

--- src/lib/hooks/useGalleryPhotos.ts
+++ src/lib/hooks/useGalleryPhotos.ts
@@ -1,34 +1,27 @@
-import {useEffect , useState } from 'react';
-import { useGlobalGivingData } from '../api';
+import { getRandomGalleryPhotos } from '../api';
+import { useState, useEffect } from 'react';
 
 interface GalleryPhotosResult {
     galleryPhotos: any[];
 }
 
 export const useGalleryPhotos = (count: number): GalleryPhotosResult => {
-    const { getRandomGalleryPhotos } = useGlobalGivingData();
     const [galleryPhotos, setGalleryPhotos] = useState<any[]>([]);
     
     useEffect(() => {
         const fetchGalleryPhotos = async () => {
             try {
-                const response = await fetch('/api/get_gallery_photos');
-                if (response.ok) {
-                    const photos = await response.json();
-                    setGalleryPhotos(photos);
-                } else {
-                    console.error('Error fetching gallery photos:', response.status)
-                }
+                const photos = await getRandomGalleryPhotos(count);
+                setGalleryPhotos(photos);
             } catch (error) {
                 console.error('Error fetching gallery photos:', error);
             }
         };
 
         fetchGalleryPhotos();
-    }, []);
+    }, [count]); // Include count in the dependency array if it's used inside the useEffect
 
-    return {galleryPhotos};
+    return { galleryPhotos };
 };
-
 
 // having useGalleryPhotos in its own file makes it easier to reuse this logic; lower chances of code acting unexpectedly because it's being duplicated

1f6e0aec01cddc664a83d8764973ee87bebcee3c
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-29 13:41:21 -0500

    Update globalgivingprox.js
    - added auth header

--- src/pages/api/globalgivingprox.js
+++ src/pages/api/globalgivingprox.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const API_KEY = process.env.NEXT_PUBLIC_GLOBAL_GIVING_API_KEY;
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const response = await axios.get('https://api.globalgiving.org' + req.url);
-    res.status(response.status).json(response.data);
+    const response = await axios.get('https://api.globalgiving.org' + req.url, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    });
   } catch (error) {
     res.status(error.response?.status || 500).json({ error: 'Internal Server Error' });
   }

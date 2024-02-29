dd872ee4cd9219f4276dbf48b19bc2ab0b411a9f
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-29 11:48:08 -0500

    Update search_page.tsx
    - fix syntax

--- src/pages/search/search_page.tsx
+++ src/pages/search/search_page.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link'
-import NavBar from '../../components/navigation'
+import Link from 'next/link';
+import NavBar from '../../components/navigation';
 import SupportBanner from '@/components/support_banner';
-import Footer from '../../components/footer'
+import Footer from '../../components/footer';
 
 export default function Search() {
     return (

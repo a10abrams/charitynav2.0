5526aaea874767b7dc6e5c68850ee31e84148dad
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-29 13:32:17 -0500

    Update globalgivingprox.js
    - add key

--- src/pages/api/globalgivingprox.js
+++ src/pages/api/globalgivingprox.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const API_KEY = process.env.NEXT_PUBLIC_GLOBAL_GIVING_API_KEY;
+
 export default async function handler(req, res) {
   try {
     const response = await axios.get('https://api.globalgiving.org' + req.url);

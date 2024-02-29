e5f8edc5f8395e256c2f1e7da8220160644bd53a
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-29 11:35:56 -0500

    Update get_gallery_photos.ts
    - logic

--- src/pages/api/get_gallery_photos.ts
+++ src/pages/api/get_gallery_photos.ts
@@ -1,13 +1,17 @@
 import { getRandomGalleryPhotos } from '../../lib/api';
 import { NextApiRequest, NextApiResponse } from 'next';
+import Axios from 'axios';
+
+const API_KEY = process.env.NEXT_PUBLIC_GLOBAL_GIVING_API_KEY;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const photos = await getRandomGalleryPhotos(5);
+  try {
+    const count = req.query.count || 5; // Default count if not provided
+    const photos = await getRandomGalleryPhotos(count);
 
-        res.status(200).json(photos);
-    } catch (error) {
-        console.error('Error fetching gallery photos:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    res.status(200).json(photos);
+  } catch (error) {
+    console.error('Error fetching gallery photos:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }

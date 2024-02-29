b4e0ac5f6a57e56539d138b063295edcadeffe0d
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-29 13:22:23 -0500

    Update api.ts
    - update axios statements
    - link to prox

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,39 +3,34 @@ import { useState, useEffect } from 'react';
 import { Project } from '../types/interfaces';
 
 const API_KEY = process.env.NEXT_PUBLIC_GLOBAL_GIVING_API_KEY;
+const apiUrl = '/api/globalgivingprox;
 
 // Fetch random gallery photos
 export const getRandomGalleryPhotos = async (count: number): Promise<any[]> => {
   try {
-    const response = await axios.get(
-      `https://api.globalgiving.org/api/public/projectservice/themes/edu/projects`,
-      {
-        headers: {
-          Authorization: API_KEY,
-        },
-        params: {
-          page: 1,
-          per_page: count,
-        },
-      }
-    );
+    const response = await axios.get(apiUrl, {
+      headers: {
+        Authorization: API_KEY,
+      },
+      params: {
+        page: 1,
+        per_page: count,
+      },
+    });
 
     // Process the response and send the appropriate data in the response
     const projects = response.data.projects;
     const randomProjects = projects.sort(() => 0.5 - Math.random()).slice(0, count);
 
     const photosPromises = randomProjects.map(async (project: Project) => {
-      const projectResponse = await axios.get(
-        `https://api.globalgiving.org/api/public/projectservice/themes/edu/projects`,
-        {
-          headers: {
-            Authorization: API_KEY,
-          },
-          params: {
-            count: 10, // CHANGE FOR TESTING--10 IS MAX ACCORDING TO API DOCS
-          },
-        }
-      );
+      const projectResponse = await axios.get(apiUrl, {
+        headers: {
+          Authorization: API_KEY,
+        },
+        params: {
+          count: 10, // CHANGE FOR TESTING--10 IS MAX ACCORDING TO API DOCS
+        },
+      });
 
       return projectResponse.data.photos[0];
     });

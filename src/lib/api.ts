cea0082d187576b809f29488535f0bbe164b2633
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-03-01 13:21:01 -0500

    Update api.ts
    - change structure of requests; trying to handle 'error not found'

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,33 +1,41 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
 import { Project } from '../types/interfaces';
 
 const API_KEY = process.env.NEXT_PUBLIC_GLOBAL_GIVING_API_KEY;
-const apiUrl = '/api/globalgivingprox';
+const apiUrl = getAbsoluteUrl('/api/globalgivingprox');
+
+const getAbsoluteUrl = (relativeUrl: string) =>
+  process.env.NODE_ENV === 'production'
+    ? 'https://charitynav-nine.vercel.app' + relativeUrl
+    : 'http://localhost:3000' + relativeUrl;
 
 // Fetch random gallery photos
-export const getRandomGalleryPhotos = async (count: number): Promise<any[]> => {
+export const getRandomGalleryPhotos = async (themes: string[], count: number): Promise<any[]> => {
   try {
+    // Use the first theme ID for the initial request
     const response = await axios.get(apiUrl, {
       headers: {
         Authorization: API_KEY,
       },
       params: {
+        themeId: themes[0],  // Use the first theme ID
         page: 1,
         per_page: count,
       },
     });
 
-    // Process the response and send the appropriate data in the response
+    // Process the response and send data
     const projects = response.data.projects;
     const randomProjects = projects.sort(() => 0.5 - Math.random()).slice(0, count);
 
     const photosPromises = randomProjects.map(async (project: Project) => {
+      // Use each theme ID for subsequent requests
       const projectResponse = await axios.get(apiUrl, {
         headers: {
           Authorization: API_KEY,
         },
         params: {
+          themeId: themes,  // Use all theme IDs
           count: 10, // CHANGE FOR TESTING--10 IS MAX ACCORDING TO API DOCS
         },
       });

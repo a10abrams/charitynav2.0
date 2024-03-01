3e260479f67aab69fde60651ce4865f2ad16f5cc
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-03-01 14:13:08 -0500

    Update globalgivingprox.js
    - manually write CORS headers; WILL BE CHANGED WHEN NPM IS ACCESSIBLE

--- src/pages/api/globalgivingprox.js
+++ src/pages/api/globalgivingprox.js
@@ -5,6 +5,13 @@ const API_KEY = process.env.NEXT_PUBLIC_GLOBAL_GIVING_API_KEY;
 
 export default async function handler(req, res) {
   try {
+    // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Allow the necessary HTTP methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    // Allow the necessary headers
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+
     const response = await axios.get('https://api.globalgiving.org' + req.url, {
       headers: {
         Authorization: API_KEY,

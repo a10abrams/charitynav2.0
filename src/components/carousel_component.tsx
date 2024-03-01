a03c31a79e43a61996841b8711a452e8d7e85f5b
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-03-01 14:01:06 -0500

    Update carousel_component.tsx
    - better photo handling
    - error message/display

--- src/components/carousel_component.tsx
+++ src/components/carousel_component.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { CarouselComponentProps } from '../types/interfaces';
 
 const CarouselComponent: React.FC<CarouselComponentProps> = ({ photos }) => {
-    return (
-        <div className = "carousel-container">
-           {photos.map((photo, index) => (
-            <img key = {index} src = {photo.url} alt = {photo.description}/>
-           ))}
-        </div>
-    );
+   return (
+      <div className="carousel-container">
+         {photos ? (
+            photos.map((photo, index) => (
+               <img key={index} src={photo.url} alt={photo.description} />
+            ))
+         ) : (
+            <p>No photos available</p>
+         )}
+      </div>
+   );
+};
+
 }
 
 export default CarouselComponent;

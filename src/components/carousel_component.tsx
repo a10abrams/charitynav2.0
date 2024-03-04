c9577a4e657dc00011d1d35643c157e255ac39b6
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-03-04 10:51:05 -0500

    Update carousel_component.tsx
    -- `photos` can be an array of strings or an array of objects with "url" and "description" (union type)

--- src/components/carousel_component.tsx
+++ src/components/carousel_component.tsx
@@ -5,12 +5,17 @@ const CarouselComponent: React.FC<CarouselComponentProps> = ({ photos }) => {
    return (
       <div className="carousel-container">
          {photos ? (
-            photos.map((photo, index) => (
-               <img key={index} src={photo.url} alt={photo.description} />
-            ))
-         ) : (
-            <p>No photos available</p>
-         )}
+                photos.map((photo, index) => (
+                    // Check if it's a string (URL) or an object with 'url' property
+                    typeof photo === 'string' ? (
+                        <img key={index} src={photo} alt={`Photo ${index}`} />
+                    ) : (
+                        <img key={index} src={photo.url} alt={photo.description} />
+                    )
+                ))
+            ) : (
+                <p>No photos available</p>
+            )}
       </div>
    );
 };
